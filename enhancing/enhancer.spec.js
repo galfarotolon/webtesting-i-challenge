201beffcd330d0775ae762cc3affedeb02e7574a
Guillermo Alfaro <galfarotolon@gmail.com>
2020-06-17 17:20:49 -0500

    GOT the 3 main functions working for tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,67 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+
+
+
+describe('repair()', function () {
+
+
+    test('should repair ', function () {
+
+        //arrange
+        const expected = 100;
+
+
+        const item = { name: 'gruktor', enhancement: '20', durability: 20 }
+        //act
+        const actual = enhancer.repair(item)
+
+        //assert
+        expect(actual).toBe(expected)
+
+    })
+})
+
+describe('succeed()', function () {
+    it('returns a new item object modified by succeeds rules', function () {
+
+        const expected = 20
+
+        const item = { name: 'gruktor', enhancement: 19, durability: 90 }
+
+        const enhance = enhancer.succeed(item)
+
+        expect(enhance).toBe(expected)
+
+    })
+})
+
+
+describe('fail()', function () {
+    it('returns a new item object modified by fail rules', function () {
+
+        const expected = { enhancement: 19, durability: 80 }
+
+        const item = { enhancement: 20, durability: 90 }
+
+        const enhance = enhancer.fail(item)
+
+        expect(enhance).toEqual(expected)
+
+    })
+})
+
+describe('get()', function () {
+
+    it('returns item name plus enhacement level', function () {
+
+        const expected = { name: '[+7] great sword', enhancement: 7 }
+        const item = { name: 'great sword', enhancement: 7 }
+
+        const newName = enhancer.get(item)
+
+        expect(newName).toEqual(expected)
+
+    })
+})
